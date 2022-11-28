1b6ae2ef7b0929db639377b6fdf41af703880f2f
Jannik Lipke <Jannik.Lipke@gmx.de>
2022-11-28 12:43:45 +0100

    Finished Github Auth

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('front/', views.FrontView.as_view(), name='front'),
-
+    path('oauth/authorize', views.oauth_authorize, name='oauth_authorize'),
+    path('oauth/callback', views.oauth_callback, name='oauth_callback'),
 ]
--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,18 +1,40 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
-from django.views import View
 from authlib.integrations.django_client import OAuth
+from django.views import View
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView, ListView, TemplateView
+
+from webapp import settings
+
+# OAuth setup
+oauth = OAuth()
+oauth.register(settings.OAUTH_CLIENT_NAME, **settings.OAUTH_CLIENT)
+
 
 # Create your views here.
 def index(request):
     return HttpResponseRedirect(reverse('front'))
 
+
 class FrontView(TemplateView):
     template_name = 'front.html'
 
+
 def oauth_authorize(request):
-    oauth = OAuth(request)
-    redirect_uri = request.build_absolute_uri(reverse('oauth_callback'))
-    return oauth.github.authorize_redirect(request, redirect_uri)
+    github = oauth.create_client(settings.OAUTH_CLIENT_NAME)
+    redirect_uri = 'http://127.0.0.1:8000/oauth/callback'
+    return github.authorize_redirect(request, redirect_uri)
+
+
+def oauth_callback(request):
+    token = oauth.github.authorize_access_token(request)
+    resp = oauth.github.get('user', token=token)
+    resp.raise_for_status()
+    profile = resp.json()
+    context = {
+        'profile': profile,
+        'token': token
+    }
+    print(context)
+    return render(request, "result.html", context)
